c40c3f90b0580925279528c646b533ebc4f8dc3c
sreeharin <sreeharinagesh@gmail.com>
2023-02-10 11:01:10 +0530

    Final update.

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import os
 from zipfile import ZipFile
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, jsonify
 from qrcode import make
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'files/'
+app.config['ZIP_FOLDER'] = 'zip/'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/convert', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def convert():
     if request.method == 'POST':
         files = request.files.getlist('files')
@@ -27,10 +25,22 @@ def convert():
                     file_name = file[:ext]
                     qr.save(f"{PATH}/{file_name}.png")
 
-        # Convert to zip, delete files and return the zip
-        # for file in os.listdir(PATH):
-            # if file.endswith('png'):
-        return 'Success!'
+        ZIP_PATH = os.path.join(app.config['ZIP_FOLDER']) + 'qr_codes.zip'
+        with ZipFile(ZIP_PATH, 'w') as _zip:
+            for file in os.listdir(PATH):
+                if file != '.gitkeep':
+                    if file.endswith('png'):
+                        _zip.write(PATH+file)
+                    os.remove(PATH+file)
+
+        return ({'result': 'success'}), 201
+    elif request.method == 'GET':
+        return render_template('index.html')
+
+@app.get('/download')
+def download():
+    return send_from_directory(
+            os.path.join(app.config['ZIP_FOLDER']), 'qr_codes.zip')
 
 if __name__ == '__main__':
     app.run(debug=True)
